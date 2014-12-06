b3613b22b3fe77e46f1f02257990e452f7c2796d
Kouta Shimomatsubae <o104064k@mail.kyutech.jp>
2014-12-06 12:38:29 +0900

    define部分の変数名の変更、コメントの追加を行った。

--- Nicoichi/Nicoichi.cpp
+++ Nicoichi/Nicoichi.cpp
@@ -1,14 +1,14 @@
 /* -------------------------------------
    pin番号指定
 ------------------------------------- */
-#define EncRPhaseA 2
-#define EncRPhaseB 3
-#define MtrR1 8
-#define MtrR2 9
-#define EncLPhaseA 4
-#define EncLPhaseB 5
-#define MtrL1 10
-#define MtrL2 11
+#define R_ENC_PHASE_A 2
+#define R_ENC_PHASE_B 3
+#define MTR_R1 8
+#define MTR_R2 9
+#define L_ENC_PHASE_A 4
+#define L_ENC_PHASE_B 5
+#define MTR_L1 10
+#define MTR_L2 11
 
 
 /* -------------------------------------
@@ -34,16 +34,16 @@ float Vel = 0.035;
 volatile long DegIniR = 0;
 
 // エンコーダー用変数
-volatile int NowEncRPhaseB; // 条件分岐の都度読み込みを実行しないように
-volatile int LastEncRPhaseB; // 上り降りで代入
+volatile int NowR_ENC_PHASE_B; // 条件分岐の都度読み込みを実行しないように
+volatile int LastR_ENC_PHASE_B; // 上り降りで代入
 
 // 速度関係
-volatile float VelR = 0; // right速度
+volatile float VelR = 0; // 現在の右輪の速度[m/s]
 volatile unsigned long LastTimeR = 0; // 前回角速度を計測した時の絶対時刻
 volatile unsigned long ElapsedTimeR = 0; // 経過時間
-float ComVelR = 0; //define right speed
-unsigned int inputR = 0;
-float max_VelR = 0;
+float ComVelR = 0; //速度の目標値[m/s]
+unsigned int inputR = 0; //速度の操作量[m/s]
+float max_VelR = 0;  //速度の最大値[m/s]
 
 /* -------------------------------------
    グローバル変数L
@@ -52,16 +52,16 @@ float max_VelR = 0;
 volatile long DegIniL = 0;
 
 // エンコーダー用変数
-volatile int NowEncLPhaseB; // 条件分岐の都度読み込みを実行しないように
-volatile int LastEncLPhaseB; // 上り降りで代入
+volatile int NowL_ENC_PHASE_B; // 条件分岐の都度読み込みを実行しないように
+volatile int LastL_ENC_PHASE_B; // 上り降りで代入
 
 // 速度関係
 volatile float VelL = 0; // left速度
 volatile unsigned long LastTimeL = 0; // 前回角速度を計測した時の絶対時刻
 volatile unsigned long ElapsedTimeL = 0; // 経過時間
-float ComVelL = 0; 
-unsigned int inputL = 0;
-float max_VelL = 0;
+float ComVelL = 0;  //速度の目標値[m/s]
+unsigned int inputL = 0;  //速度の操作量[m/s]
+float max_VelL = 0;  //速度の最大値[m/s]
 
 /* -------------------------------------
    関数プロトタイプ宣言
@@ -74,34 +74,34 @@ void EncCounterL ( void );
    メイン関数
 ------------------------------------- */
 void setup () {
-  pinMode ( EncRPhaseA, INPUT );
-  pinMode ( EncRPhaseB, INPUT );
-  pinMode(MtrR1, OUTPUT);
-  pinMode(MtrR2, OUTPUT);
-  pinMode ( EncLPhaseA, INPUT );
-  pinMode ( EncLPhaseB, INPUT );
-  pinMode(MtrL1, OUTPUT);
-  pinMode(MtrL2, OUTPUT);
+  pinMode ( R_ENC_PHASE_A, INPUT );
+  pinMode ( R_ENC_PHASE_B, INPUT );
+  pinMode(MTR_R1, OUTPUT);
+  pinMode(MTR_R2, OUTPUT);
+  pinMode ( L_ENC_PHASE_A, INPUT );
+  pinMode ( L_ENC_PHASE_B, INPUT );
+  pinMode(MTR_L1, OUTPUT);
+  pinMode(MTR_L2, OUTPUT);
   
   Serial.begin ( 115200 );
 
-  LastEncRPhaseB = digitalRead ( EncRPhaseB ); // 初期化
-  LastEncLPhaseB = digitalRead ( EncLPhaseB ); // 初期化
+  LastR_ENC_PHASE_B = digitalRead ( R_ENC_PHASE_B ); // 初期化
+  LastL_ENC_PHASE_B = digitalRead ( L_ENC_PHASE_B ); // 初期化
   
   attachInterrupt ( 2, EncRCounter, CHANGE ); // 割り込み処理登録
   attachInterrupt ( 4, EncLCounter, CHANGE ); // 割り込み処理登録
   
   ComVelR = TREAD * omega + Vel;
   ComVelL = Vel - TREAD * omega;
-  max_VelR = ((5200.0/127.78/60.0) * PI * R_WHEEL)*MAX_MTR_R2/255.0;
+  max_VelR = ((5200.0/127.78/60.0) * PI * R_WHEEL)*MAX_MTR_R2/255.0;   //((毎分回転数/ギア比/秒)*pi*右輪直径)/最大速度時のパルス
   max_VelL = ((5200.0/127.78/60.0) * PI * L_WHEEL)*MAX_MTR_L2/255.0;
  
   
   analogWriteResolution(8);
-  digitalWrite(MtrR1, LOW);
-  analogWrite(MtrR2, 0);
-  digitalWrite(MtrL1, HIGH);
-  analogWrite(MtrL2, 255);
+  digitalWrite(MTR_R1, LOW);
+  analogWrite(MTR_R2, 0);  //右の入力値はそのままの値(255で100%)
+  digitalWrite(MTR_L1, HIGH);
+  analogWrite(MTR_L2, 255);  //左の入力値は255-操作量(0で100%)
 
 }
 
@@ -122,8 +122,8 @@ void loop () {
    else if(inputL < 0){
     inputL = 0; 
    }
-   analogWrite(MtrR2, inputR);
-   analogWrite(MtrL2, 255 - inputL);
+   analogWrite(MTR_R2, inputR);
+   analogWrite(MTR_L2, 255 - inputL);
    
   Serial.print(inputR ,DEC);
   Serial.print("\t");
@@ -137,16 +137,16 @@ void loop () {
 ------------------------------------- */
 void EncRCounter ( void ) {
   ElapsedTimeR = millis() -LastTimeR; // 経過時間の算出
-  NowEncRPhaseB = digitalRead ( EncRPhaseB );
-  if ( digitalRead(EncRPhaseA) == HIGH ) {
+  NowR_ENC_PHASE_B = digitalRead ( R_ENC_PHASE_B );
+  if ( digitalRead(R_ENC_PHASE_A) == HIGH ) {
     // この下の代入は現在のBを読んでいるだけ
      // 条件分岐の都度に digitalRead を実行しないように、ここで最初に一回だけ読み込んで NewEncPhaseB として置いておく
-    switch(LastEncRPhaseB){ // 前回チェンジ時のBで分岐
-    case 0: switch(NowEncRPhaseB){
+    switch(LastR_ENC_PHASE_B){ // 前回チェンジ時のBで分岐
+    case 0: switch(NowR_ENC_PHASE_B){
             case 0: DegIniR -1; break;
             case 1: DegIniR += 4;     break;
             }  break; 
-    case 1: switch(NowEncRPhaseB){
+    case 1: switch(NowR_ENC_PHASE_B){
             case 0: DegIniR -= 4;  break;
             case 1: DegIniR ++;  break;
             }  break;
@@ -157,24 +157,21 @@ void EncRCounter ( void ) {
   DegIniR = 0;
   
   LastTimeR = ElapsedTimeR;			   
-  LastEncRPhaseB = digitalRead ( EncRPhaseB ); // 現在のBを改めて残す
-//  Serial.print(NowEncRPhaseB ,DEC);
-//  Serial.print("\t");
-//  Serial.println(NowEncLPhaseB ,DEC);
+  LastR_ENC_PHASE_B = digitalRead ( R_ENC_PHASE_B ); // 現在のBを改めて残す
 }
 
 void EncLCounter ( void ) {
   ElapsedTimeL = millis() -LastTimeL; // 経過時間の算出
-  NowEncLPhaseB = digitalRead ( EncLPhaseB );
-  if ( digitalRead(EncLPhaseA) == HIGH ) {
+  NowL_ENC_PHASE_B = digitalRead ( L_ENC_PHASE_B );
+  if ( digitalRead(L_ENC_PHASE_A) == HIGH ) {
     // この下の代入は現在のBを読んでいるだけ
      // 条件分岐の都度に digitalRead を実行しないように、ここで最初に一回だけ読み込んで NewEncPhaseB として置いておく
-    switch(LastEncLPhaseB){ // 前回チェンジ時のBで分岐
-    case 0: switch(NowEncLPhaseB){
+    switch(LastL_ENC_PHASE_B){ // 前回チェンジ時のBで分岐
+    case 0: switch(NowL_ENC_PHASE_B){
             case 0: DegIniL -1; break;
             case 1: DegIniL += 4;     break;
             }  break; 
-    case 1: switch(NowEncLPhaseB){
+    case 1: switch(NowL_ENC_PHASE_B){
             case 0: DegIniL -= 4;  break;
             case 1: DegIniL ++;  break;
             }  break;
@@ -185,8 +182,5 @@ void EncLCounter ( void ) {
   DegIniL = 0;
   
   LastTimeL = ElapsedTimeL;			   
-  LastEncLPhaseB = digitalRead ( EncLPhaseB ); // 現在のBを改めて残す
-//  Serial.print(NowEncRPhaseB ,DEC);
-//  Serial.print("\t");
-//  Serial.println(NowEncLPhaseB ,DEC);
+  LastL_ENC_PHASE_B = digitalRead ( L_ENC_PHASE_B ); // 現在のBを改めて残す
 }
